fd7894cc8f5dddb217e0ce4b0ab6a05a14e53df4
Shapna <shapna088@gmail.com>
2022-11-25 16:47:36 +0600

    add

--- index.js
+++ index.js
@@ -56,6 +56,15 @@ async function run() {
             res.send(result);
         })
 
+        //Get My Bookings Data API
+        app.get('/bookings', async(req, res) => {
+            const email = req.query.email;
+            const query = {email: email}
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
+
     } finally {
         
     }
